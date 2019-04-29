7e8c7e67e9c9fc079ba5f8c28e9eab55a4fbb106
tony <tonytenorioc@gmail.com>
2019-04-28 20:44:34 -0600

    Gallery effects section completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+document.querySelectorAll('.navbar .nav-link').forEach(enlace => {
+    enlace.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
 $(document).ready(function(){
+
+    $('.nav-link').on('click', function(e){
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop:$(hash).offset().top
+        }, 1000, function(){
+          window.location.hash = hash
+        })
+    });
+
+    $('.gallery-list-item').click(function() {
+        let value = $(this).attr('data-filter');
+        if(value === 'all') {
+          $('.filter').show(300);
+        } else {
+          $('.filter').not('.' + value).hide(300);
+          $('.filter').filter('.' + value).show(300);
+        }
+    });
+    
+    $('.gallery-list-item').click(function() {
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
+
     $('#date').text(new Date().getFullYear());
 });
