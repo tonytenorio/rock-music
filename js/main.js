e364363584f0d324e2d22102dfcabfe25cf18727
tony <tonytenorioc@gmail.com>
2019-04-29 18:26:09 -0600

    Update the main.js File

--- js/main.js
+++ js/main.js
@@ -11,16 +11,6 @@ document.querySelectorAll('.navbar .nav-link').forEach(enlace => {
 
 $(document).ready(function(){
 
-    $('.nav-link').on('click', function(e){
-        e.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop:$(hash).offset().top
-        }, 1000, function(){
-          window.location.hash = hash
-        })
-    });
-
     $('.gallery-list-item').click(function() {
         let value = $(this).attr('data-filter');
         if(value === 'all') {
